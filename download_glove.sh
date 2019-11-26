20eaa91a910492c1b3e7d25ce87d92aaefb220af
wrhuang <Ronny.Huang@ey.com>
2019-11-26 13:32:09 -0500

    fixed bugs now it works

--- download_glove.sh
+++ download_glove.sh
@@ -3,5 +3,7 @@ if [[ ! -d glove ]]; then
 fi
 
 cd glove
-wget http://nlp.stanford.edu/data/wordvecs/glove.42B.300d.zip
-unzip glove.42B.300d.zip
+#wget http://nlp.stanford.edu/data/wordvecs/glove.42B.300d.zip
+wget http://nlp.stanford.edu/data/glove.6B.zip
+#unzip glove.42B.300d.zip
+unzip glove.6B.zip
---
+++ download_stanford_tokenizer.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+wget https://nlp.stanford.edu/software/stanford-postagger-2018-10-16.zip
+unzip stanford-postagger-2018-10-16.zip -d /root/datasets/
